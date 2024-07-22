2ca50153cd4916275a613128480b4a1ea2f7552a
Abhi Jeet <ak806252@gmail.com>
2024-07-22 16:50:22 +0530

    handled case of empty caption

--- main.py
+++ main.py
@@ -137,8 +137,7 @@ def is_valid_email(email):
     regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     
     # Use re.match to check if the email matches the regex
-    return re.match(regex, email) is not None
-
+    return re.match(regex, email) is not None if email else False
 def send_attachments():
     global attachments, current_receiver_email, timer
 
